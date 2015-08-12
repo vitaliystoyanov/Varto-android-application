0faf62b0f54c84f9e4efa4d71a443368e572ce46
Vitaliy Stoyanov <vitaliy34911@gmail.com>
2015-08-12 18:18:03 +0300

    Added class DBConstants.

---
+++ app/src/main/java/es/esy/varto_novomyrgorod/varto/model/database/DBConstants.java
@@ -0,0 +1,21 @@
+package es.esy.varto_novomyrgorod.varto.model.database;
+
+public class DBConstants {
+    public static final String TAG_TABLE_NEWS = "news";
+    public static final String TAG_TABLE_SCHEDULE = "schedule";
+
+    public static final String TAG_ID = "id";
+    public static final String TAG_SHOP = "shop";
+    public static final String TAG_TITLE = "title";
+    public static final String TAG_IMAGE = "image";
+    public static final String TAG_ARTICLE = "article";
+    public static final String TAG_CREATED_AT = "created_at";
+
+    public static final String TAG_SUNDAY = "sunday";
+    public static final String TAG_MONDAY = "monday";
+    public static final String TAG_TUESDAY = "tuesday";
+    public static final String TAG_WEDNESDAY = "wednesday";
+    public static final String TAG_THURSDAY = "thursday";
+    public static final String TAG_FRIDAY = "friday";
+    public static final String TAG_SATURDAY = "saturday";
+}
